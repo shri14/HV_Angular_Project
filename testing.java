9b4b6b04dc0952aaad763f26db85a3464524c8c4
Preran-D <preran.dhananjaya@gmail.com>
2024-02-06 19:51:03 +0530

    footer-update msg

--- testing.java
+++ testing.java
@@ -3,11 +3,6 @@ public class testing {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("world");
-        System.out.println("world");
-        System.out.println("world");
-        System.out.println("world");
-        System.out.println("world");
-        System.out.println("world");
 
     }
 }
