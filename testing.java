96ab47cf4f51d58d3b915767ea258ad67479227a
Preran-D <preran.dhananjaya@gmail.com>
2024-02-06 20:25:50 +0530

    testing -10

--- testing.java
+++ testing.java
@@ -2,8 +2,8 @@ public class testing {
 
     public static void main(String[] args) {
         System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
+        System.out.println("Nice");
+        System.out.println("Nice");
 
 
 
