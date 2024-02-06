940540281de9c746e606be56e47e7b9ff5b57dc6
Roopesh <roopeshbabubh@gmail.com>
2024-02-06 20:19:46 +0530

    header-first-4

--- testing.java
+++ testing.java
@@ -2,13 +2,6 @@ public class testing {
 
     public static void main(String[] args) {
         System.out.println("hello");
-        System.out.println("hello");
-        System.out.println("hello");
-
-
-
-
-
 
     }
 }
